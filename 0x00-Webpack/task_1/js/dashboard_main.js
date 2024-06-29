3148894435ad69006380abccad7dcfcb3b545316
edzordzinam44 <edzordzinam.alipui@gmail>
2024-06-29 16:32:48 +0000

    Added files

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+import { debounce } from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+function updateCounter () {
+	count += 1;
+	$('#count').text(`${count} clicks on the button`);
+}
+
+$('button').on('click', debounce(updateCounter, 300));
