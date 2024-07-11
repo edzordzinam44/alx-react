2d3a676895d29efbae02d6c7c804bf3d3a267947
edzordzinam44 <edzordzinam.alipui@gmail>
2024-07-11 04:06:56 +0000

    Modify the app

---
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import Notifications from './Notifications';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const notificationsRoot = ReactDOM.createRoot(document.getElementById('root-notifications'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Render the notifications component with React.StrictMode
+<div>
+notificationsRoot.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
+</div>
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
