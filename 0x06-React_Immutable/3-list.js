6f5821a477b2de5c45ba18a2c2fc9144bea27423
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-07-19 12:57:16 +0000

    List and push

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,9 +1,9 @@
 import { List } from 'immutable';
 
 export function getListObject(object) {
-    return object.get('list');
+  return object.get('list');
 }
 
 export function addElementToList(list, element) {
-    return list.push(element);
+  return list.push(element);
 }
