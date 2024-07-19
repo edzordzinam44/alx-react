583c2aa201953904a9fcfa2fdd9857cfe4769e21
edzordzinam44 <edzordzinam.alipui@gmail>
2024-07-19 11:32:30 +0000

    changes

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,8 +1,5 @@
-import { fromJS } from "immutable";
+import { fromJS } from 'immutable';
 
 export default function getImmutableObject(object) {
   return fromJS(object);
 }
-
-// Export the function so it can be used in other files
-module.exports = getImmutableObject;
