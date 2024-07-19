f67e18781b7cfc92844799edc9266319a498f00b
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-07-19 12:12:16 +0000

    Accessing nested elements

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,7 @@
+import { fromJS } from 'immutable';
+
+export default function accessImmutableObject(object, array) {
+  const immutableMap = fromJS(object);
+
+  return immutableMap.getIn(array);
+}
