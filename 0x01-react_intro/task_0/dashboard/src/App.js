64b0f67e7a8ff74c12d3b36217de612d3d29fa28
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-08-18 14:38:57 +0000

    Updated the react app body

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import logo from './holberton_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="Logo" />
+        <h1>School Holberton</h1>
       </header>
+      <hr id="hr" />
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+      </main>
+      <footer className="App-footer">
+        <hr id="hr-footer" />
+        <p className="footer-p">Copyright 2020 - Holberton School</p>
+      </footer>
     </div>
   );
 }
